bab275ce2f22530dae0a7afa197d03ef3ae99880
hkkh2004 <69750816+hkkh2004@users.noreply.github.com>
2021-01-08 23:25:58 +0530

    Add files via upload

---
+++ dictionaryEX.py
@@ -0,0 +1,13 @@
+#in future update this to a better dictionary where it can find meanings of words throuhg wikipedia or some text.
+
+
+words_dictionary = {"ironic":"happening in a way contrary to what is expected, and typically causing wry amusement because of this.",
+                    "enormity":"the great or extreme scale, seriousness, or extent of something perceived as bad or morally wrong.",
+                    "disinterested":"having or feeling no interest in something; uninterested."
+                     }
+
+
+user_input = input("type the word here : ")
+
+
+print(words_dictionary[user_input])                    
